f935d6b7d359bdc5205df08747cd9229f1e149a2
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-08-08 19:04:02 +0200

    v0.2.2

--- src/netstandard2.0/Root/XReceived.cs
+++ src/netstandard2.0/Root/XReceived.cs
@@ -10,17 +10,22 @@
   {
     public static void Only(Action action)
     {
+      var query = new Query(SubstitutionContext.Current.CallSpecificationFactory);
+      SubstitutionContext.Current.ThreadContext.RunInQueryContext(action, query);
+
       new SequenceExclusiveAssertion(
         new FilterAllowingPropertyGetters()
-        ).Assert(SubstitutionContext.Current.RunQuery(action));
+        ).Assert(query.Result());
     }
 
     public static void Only(Action action, IQueryFilter methodFilter)
     {
+      var query = new Query(SubstitutionContext.Current.CallSpecificationFactory);
+      SubstitutionContext.Current.ThreadContext.RunInQueryContext(action, query);
+
       new SequenceExclusiveAssertion(
         Allow.AllOf(Allow.PropertyGetters(), methodFilter)
-        ).Assert(SubstitutionContext.Current.RunQuery(action));
-      //bug do not use obsolete API. Instead: https://github.com/nsubstitute/NSubstitute/blob/3446605c077438bef91c349a5249a12644ffca37/src/NSubstitute/Received.cs
+        ).Assert(query.Result());
     }
   }
 }
